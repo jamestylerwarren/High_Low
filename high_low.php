fdf0262bcdc9cb2eb62fc03e3adfe616c895a742
Tyler Warren <warrejt@yahoo.com>
2016-07-05 10:33:47 -0500

    updating using argv and argc

--- high_low.php
+++ high_low.php
@@ -1,17 +1,23 @@
 <?php
 
-//random number generator
-$num = mt_rand(1,100);
-$guesses = 0;
+// var_dump($argc);
+// var_dump($argv);
 
 
-do {
-	fwrite(STDOUT, 'Guess? ') . PHP_EOL;
-	$guess = trim(fgets(STDIN));
-	$guesses += 1;
-	if ($num == $guess){
-		echo "Good Guess! You guessed $guesses times." . PHP_EOL;
-	} elseif ($num > $guess) {
-		echo 'Higher' . PHP_EOL;
-	} else echo 'Lower' . PHP_EOL;
-} while ($num != $guess);
+if ($argc === 3 && is_numeric($argv[1]) && is_numeric($argv[2])) {
+
+	$num = mt_rand($argv[1],$argv[2]);
+	$guesses = 0;
+
+	do {
+		fwrite(STDOUT, 'Guess? ') . PHP_EOL;
+		$guess = trim(fgets(STDIN));
+		$guesses += 1;
+		if ($num == $guess){
+			echo "Good Guess! You guessed $guesses times." . PHP_EOL;
+		} elseif ($num > $guess) {
+			echo "Higher" . PHP_EOL;
+		} else echo "Lower" . PHP_EOL;
+	} while ($num != $guess);
+
+} else echo "Please pass two numbers" . PHP_EOL;
