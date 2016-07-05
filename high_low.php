c546a670e934a17971ac7c73d134baa869924741
Tyler Warren <warrejt@yahoo.com>
2016-07-05 11:11:22 -0500

    high low game modifications

--- high_low.php
+++ high_low.php
@@ -1,23 +1,30 @@
 <?php
 
-// var_dump($argc);
-// var_dump($argv);
+$min = 1;
+$max = 100;
 
 
-if ($argc === 3 && is_numeric($argv[1]) && is_numeric($argv[2])) {
+if ($argc >= 3) {
+	if (is_numeric($argv[1]) && is_numeric($argv[2]) && $argv[1] < $argv[2]) {
+		$min = $argv[1];
+		$max = $argv[2];
+	} else {
+		echo "Please pass two numberic values, the first being the minimum value, the second being the maximum value" . PHP_EOL;
+		exit(1);
+	}
+} 
 
-	$num = mt_rand($argv[1],$argv[2]);
-	$guesses = 0;
+$num = mt_rand($min,$max);
+$guesses = 0;
 
-	do {
-		fwrite(STDOUT, 'Guess? ') . PHP_EOL;
-		$guess = trim(fgets(STDIN));
-		$guesses++;
-		if ($num == $guess){
-			echo "Good Guess! You guessed $guesses times." . PHP_EOL;
-		} elseif ($num > $guess) {
-			echo "Higher" . PHP_EOL;
-		} else echo "Lower" . PHP_EOL;
-	} while ($num != $guess);
 
-} else echo "Please pass two numbers" . PHP_EOL;
+do {
+	fwrite(STDOUT, 'Guess? ') . PHP_EOL;
+	$guess = trim(fgets(STDIN));
+	$guesses++;
+	if ($num == $guess){
+		echo "Good Guess! You guessed $guesses times." . PHP_EOL;
+	} elseif ($num > $guess) {
+		echo "Higher" . PHP_EOL;
+	} else echo "Lower" . PHP_EOL;
+} while ($num != $guess);
